aaebf767f96e23175bb7152b6b778f5625712841
CLOCKWORK95 <gimbors95@gmail.com>
2022-02-09 18:50:36 +0100

    first release

--- models.py
+++ models.py
@@ -2,33 +2,43 @@
 import tensorflow as tf
 
 
-def tuning_model( opt, fsize1, neurons_1, act, input_shape ) :
-    # Creazione del Modello CNN da tunare ( Iper-Parametri )
+def tuning_model( neurons, exponent, ksize, numf, act, input_shape ) :
+    # Creazione del Modello CNN da tunare ( Iper-Parametri ) [ BigJohn ]
+
     model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Conv1D(filters = 128, kernel_size = fsize1, activation='relu', padding="same", input_shape = input_shape,
-                    kernel_regularizer = tf.keras.regularizers.l2(0.001)))
-    model.add(tf.keras.layers.AveragePooling1D( pool_size= 2 , padding="same") )
+    model.add(tf.keras.layers.Conv1D(   filters = numf, kernel_size = ksize, activation='relu', padding = "same", input_shape = input_shape, 
+                                        kernel_regularizer = tf.keras.regularizers.l2(1.e-4)))
+    model.add(tf.keras.layers.AveragePooling1D( pool_size= 8, strides = 5, padding = "same") )
     model.add(tf.keras.layers.BatchNormalization())
 
-    model.add(tf.keras.layers.Conv1D(filters = 64, kernel_size = 5,  activation='relu', padding="same", input_shape = input_shape))
-    model.add(tf.keras.layers.AveragePooling1D( pool_size= 2 , padding="same") )
+    model.add(tf.keras.layers.Conv1D(   filters = int(numf/exponent), kernel_size = int(ksize/exponent), activation='relu', padding = "same" , 
+                                        kernel_regularizer = tf.keras.regularizers.l2(1.e-4)))
+    model.add(tf.keras.layers.AveragePooling1D( pool_size = 8, strides = 5, padding = "same") )
     model.add(tf.keras.layers.BatchNormalization())
-
-    model.add(tf.keras.layers.Conv1D(filters = 32, kernel_size = 5,  activation='relu', padding="same", input_shape = input_shape))
-    model.add(tf.keras.layers.AveragePooling1D( pool_size= 2 , padding="same") )
-    #model.add(tf.keras.layers.BatchNormalization())
 
     model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense( neurons_1, activation = act ))
-    model.add(tf.keras.layers.Dropout(.45))
+
+    model.add(tf.keras.layers.Dense(    neurons , activation = act, 
+                                        kernel_regularizer = tf.keras.regularizers.l2(1.e-4)))
+    model.add(tf.keras.layers.Dropout(.2))
     model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.Dense( neurons_1/2, activation = act ))
-    model.add(tf.keras.layers.Dropout(.25))
+
+    model.add(tf.keras.layers.Dense(    int(neurons/(2**exponent)), activation = act, 
+                                        kernel_regularizer = tf.keras.regularizers.l2(1.e-4)))
+    model.add(tf.keras.layers.Dropout(.1)) 
     model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.Dense( neurons_1/4, activation = act ))
-    model.add(tf.keras.layers.Dense( 8 , activation = 'softmax' ))
+
+    model.add(tf.keras.layers.Dense(    int(neurons/(2**(exponent+1))), activation = act, 
+                                        kernel_regularizer = tf.keras.regularizers.l2(1.e-4)))
+
+    model.add(tf.keras.layers.Dense( 8 , activation = 'softmax',  kernel_regularizer = tf.keras.regularizers.l2(1.e-4) ))
     
+    #model.summary()
+
+    opt = tf.keras.optimizers.Adam()
     model.compile( optimizer = opt, loss = "categorical_crossentropy", metrics = ['accuracy'] )
+
+   
 
     return model
 
@@ -111,6 +121,7 @@ def HyperBigJohn( input_shape ) :
     model.compile( optimizer = opt, loss = "categorical_crossentropy", metrics = ['accuracy'] )
 
     return model
+
 
 def LittleJohn( input_shape ) :
 
--- tuning.py
+++ tuning.py
@@ -4,33 +4,66 @@ from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.experimental import enable_halving_search_cv
 from sklearn.model_selection import StratifiedKFold, cross_val_score, GridSearchCV, HalvingGridSearchCV
 from models import tuning_model
+import numpy as np
+from utilities import csv_readnstack, csv_to_onehot, data_augmentation
 
 
-def hyperparams_tuning( train_x, train_y ):
+def hyperparams_tuning( train_x, train_y, input_shape ):
+
     # Tuning degli iperparametri in Cross Validation
     model = KerasClassifier( build_fn = tuning_model, verbose = 1 )
-    batch_size = [ 64 ]
+    batch_size = [ 256 ]
     opt = ['adam']
-    fsize1 = [ 15 ]
-    neurons_1 = [ 128 ]
+    neurons = [ 256, 512 ]
+    exponent = [ 1, 2 ]
+    numf = [ 64, 32 ]
+    ksize = [ 10 ]
     epochs = [ 80 ]
-    act = ['tanh']
+    act = ['tanh', 'relu']
+    input_shape = [ input_shape ]
 
     param_grid = dict(  
                         batch_size = batch_size,
+                        input_shape = input_shape,
                         epochs = epochs, 
-                        opt = opt, 
-                        fsize1 = fsize1,
+                        ksize = ksize,
+                        exponent = exponent,
+                        numf = numf,
                         act = act,
-                        neurons_1 = neurons_1 )
+                        neurons = neurons )
 
     grid = HalvingGridSearchCV( estimator = model,
                                 param_grid = param_grid,
                                 n_jobs = -1,
                                 cv = 5)
 
-    callback = tf.keras.callbacks.EarlyStopping( monitor = 'accuracy', restore_best_weights = True, patience = 30 )
+    callback = tf.keras.callbacks.EarlyStopping( monitor = 'accuracy', restore_best_weights = True, patience = 20 )
 
     grid_result = grid.fit( train_x, train_y, callbacks = [callback] )
     # Stampa del best Score dalla Grid Search
     print( "Best: %f using %s" % ( grid_result.best_score_, grid_result.best_params_ ) )
+
+
+
+def tune_hyper_parameters():
+
+    dataset_values = csv_readnstack( [ "train_gesture_x.csv", "train_gesture_y.csv", "train_gesture_z.csv" ] )
+    dataset_labels = csv_to_onehot( "train_label.csv" )
+
+    # Concatazione di nuove entries al dataset ottenute tramite Data Augmentation
+    augmentation_values, augmentation_labels = data_augmentation()
+    dataset_values = np.vstack( (dataset_values, augmentation_values) )
+    dataset_labels = np.vstack( (dataset_labels, augmentation_labels) )
+    print( dataset_values.shape )
+    print( dataset_labels.shape )
+
+    # Input Shape settings ( dimensione vettore, numero di canali )
+    input_shape = ( dataset_values.shape[1], dataset_values.shape[2] )
+
+    hyperparams_tuning( dataset_values, dataset_labels, input_shape )
+
+
+
+if __name__ == '__main__':
+    tune_hyper_parameters()
+    
